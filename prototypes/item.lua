240184730f179ff6c790e411f7d6a5543200e67d
A2noh <juan.bohacek@gmail.com>
2025-04-05 18:24:03 +0200

    fixed naming for factorio mod portal

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -4,7 +4,7 @@ data:extend(
             -- Add a packed Delivery Capsule item
             type = "item",
             name = "delivery-cannon-capsule-packed",
-            icon = "__SE-FireEverything-2.0__/graphics/icons/delivery-cannon-capsule-packed.png",
+            icon = "__SE-FireEverything-2__/graphics/icons/delivery-cannon-capsule-packed.png",
             icon_size = 64,
             subgroup = "intersurface-part",
             stack_size = 1
